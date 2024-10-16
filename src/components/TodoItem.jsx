800b00e145b9158e4efd5952fbc8b8294f251bbe
imyeremin <eremipsch@mail.ru>
2024-10-16 10:21:28 +0300

    react-redux todo

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { removeTodo, toggleTodoComplete } from "../store/todoSlise";
+
+const TodoItem = ({ id, text, completed}) => {
+  const dispatch = useDispatch();
+
+
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={() => toggleTodoComplete(id)}
+      />
+      <span>{text}</span>
+      <span
+        style={{
+          color: "red",
+          cursor: "pointer",
+          marginLeft: "10px",
+        }}
+        onClick={() => dispatch(removeTodo({id}))}
+      >
+        &times;
+      </span>
+    </li>
+  );
+};
+
+export default TodoItem;
